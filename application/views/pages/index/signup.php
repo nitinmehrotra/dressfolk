5b20ee1cdd9cbd45f88cdd09a4b7dd9408746de9
Arpit Maheshwari <arp19690@gmail.com>
2015-05-24 17:18:05 +0530

    forgot password page and signup page, boxes shifted to middle of the page

--- application/views/pages/index/signup.php
+++ application/views/pages/index/signup.php
@@ -15,63 +15,65 @@
         </div>
         <div class="container">
             <div class="row show-grid">
-                <div class="col-main">
-                    <div class="account-create">
-                        <div class="page-title">
-                            <h1>Create an Account</h1>
-                        </div>
-                        <form action="" method="post" id="form-validate">
-                            <div class="fieldset">
-                                <h2 class="legend">Personal Information</h2>
-                                <ul class="form-list">
-                                    <li class="fields">
-                                        <div class="customer-name">
-                                            <div class="field name-fullname">
-                                                <label for="fullname" class="required"><em>*</em>Full Name</label>
-                                                <div class="input-box">
-                                                    <input type="text" id="fullname" name="fullname" value="" title="Full Name" maxlength="255" class="input-text required-entry"  />
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="col-main">
+                        <div class="account-create">
+                            <div class="page-title">
+                                <h1>Create an Account</h1>
+                            </div>
+                            <form action="" method="post" id="form-validate">
+                                <div class="fieldset">
+                                    <h2 class="legend">Personal Information</h2>
+                                    <ul class="form-list">
+                                        <li class="fields">
+                                            <div class="customer-name">
+                                                <div class="field name-fullname">
+                                                    <label for="fullname" class="required"><em>*</em>Full Name</label>
+                                                    <div class="input-box">
+                                                        <input type="text" id="fullname" name="fullname" value="" title="Full Name" maxlength="255" class="input-text required-entry"  />
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <label for="email_address" class="required"><em>*</em>Email Address</label>
-                                        <div class="input-box">
-                                            <input type="email" name="email" id="email_address" value="" title="Email Address" class="input-text validate-email required-entry" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <label for="password" class="required"><em>*</em>Password</label>
-                                        <div class="input-box">
-                                            <input type="password" name="password" id="password" value="" title="Password" class="input-text required-entry" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <label for="contact" class="required"><em>*</em>Mobile</label>
-                                        <div class="input-box">
-                                            <input type="contact" name="contact" id="contact" value="" title="Mobile" maxlength="10" class="input-text required-entry" />
-                                        </div>
-                                    </li>
-                                    <li class="signup-gender-radio">
-                                        <label for="gender" class="required"><em>*</em>Gender</label>
-                                        <div class="input-box">
-                                            <input type="radio" name="gender" value="male" class="input-text" />Male&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                            <input type="radio" name="gender" checked="checked" value="female" class="input-text" />Female
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="buttons-set">
-                                <p class="required">* Required Fields</p>
-                                <p class="back-link"><a href="<?php echo goBack(); ?>" class="back-link"><small>&laquo; </small>Back</a></p>
-                                <button type="submit" title="Signup" class="button"><span><span>Submit</span></span></button>
-                            </div>
-                        </form>
-                        <script type="text/javascript">
-                            //<![CDATA[
-                            var dataForm = new VarienForm('form-validate', true);
-                            //]]>
-                        </script>
+                                        </li>
+                                        <li>
+                                            <label for="email_address" class="required"><em>*</em>Email Address</label>
+                                            <div class="input-box">
+                                                <input type="email" name="email" id="email_address" value="" title="Email Address" class="input-text validate-email required-entry" />
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <label for="password" class="required"><em>*</em>Password</label>
+                                            <div class="input-box">
+                                                <input type="password" name="password" id="password" value="" title="Password" class="input-text required-entry" />
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <label for="contact" class="required"><em>*</em>Mobile</label>
+                                            <div class="input-box">
+                                                <input type="contact" name="contact" id="contact" value="" title="Mobile" maxlength="10" class="input-text required-entry" />
+                                            </div>
+                                        </li>
+                                        <li class="signup-gender-radio">
+                                            <label for="gender" class="required"><em>*</em>Gender</label>
+                                            <div class="input-box">
+                                                <input type="radio" name="gender" value="male" class="input-text" />Male&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <input type="radio" name="gender" checked="checked" value="female" class="input-text" />Female
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="buttons-set">
+                                    <p class="required">* Required Fields</p>
+                                    <p class="back-link"><a href="<?php echo goBack(); ?>" class="back-link"><small>&laquo; </small>Back</a></p>
+                                    <button type="submit" title="Signup" class="button"><span><span>Submit</span></span></button>
+                                </div>
+                            </form>
+                            <script type="text/javascript">
+                                //<![CDATA[
+                                var dataForm = new VarienForm('form-validate', true);
+                                //]]>
+                            </script>
+                        </div>
                     </div>
                 </div>
             </div>
