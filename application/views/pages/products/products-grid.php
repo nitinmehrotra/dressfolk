0ed9a7cca09b8feda5e08a6369e2e9923e7e3e58
Arpit Maheshwari <arp19690@gmail.com>
2015-07-28 19:07:34 +0530

    search page, sorter separated

--- application/views/pages/products/products-grid.php
+++ application/views/pages/products/products-grid.php
@@ -432,61 +432,7 @@
                                     <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
                                         <div class="row">
                                             <div class="col-lg-9">
-                                                <div class="sorter_wrap">
-                                                    <div class="sort-by">
-                                                        <div class="select-new">
-                                                            <div class="select-inner">
-                                                                <span>Sort by: </span>
-                                                                <div class="overwrite-sortby">Position</div>
-                                                                <ul class="sort_by">
-                                                                    <li>
-                                                                        <a href="women-dir=asc&order=position.html">Position</a>
-                                                                    </li>
-                                                                    <li>
-                                                                        <a href="women-dir=asc&order=name.html">Name</a>
-                                                                    </li>
-                                                                    <li>
-                                                                        <a href="women-dir=asc&order=price.html">Price</a>
-                                                                    </li>
-                                                                    <li>
-                                                                        <a href="women-dir=asc&order=color.html">Color</a>
-                                                                    </li>
-                                                                    <li>
-                                                                        <a href="women-dir=asc&order=size.html">Size</a>
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </div>
-                                                        <div class="direction-list">
-                                                            <a class="direction direction-up"
-                                                               href="women-dir=desc&order=position.html"
-                                                               title="Set Descending Direction"><i
-                                                                    class="fa fa-arrow-down"></i></a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="limiter_wrap">
-                                                    <div class="limiter">
-                                                        <div class="select-new">
-                                                            <div class="select-inner">
-                                                                <div class="overwrite-limiter">12 items/page</div>
-                                                                <ul class="limiter">
-                                                                    <li>
-                                                                        <a href="women-limit=12.html"
-                                                                           class="">12</a>
-                                                                    </li>
-                                                                    <li>
-                                                                        <a href="women-limit=24.html"
-                                                                           class="">24</a>
-                                                                    </li>
-                                                                    <li>
-                                                                        <a href="women-limit=36.html"
-                                                                           class="">36</a>
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </div>                                 </div>
-                                                </div>
+                                                <?php $this->load->view('pages/products/search-sorter-wrap'); ?>
                                             </div>
                                             <div class="col-lg-3">
                                                 <div class="pager">
