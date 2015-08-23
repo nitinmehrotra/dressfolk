45f7bd2160f3ef164667e0c2afabc72a64037abe
Arpit Maheshwari <arp19690@gmail.com>
2015-08-23 17:13:16 +0530

    footer section changes

--- application/views/layout/front/footer.php
+++ application/views/layout/front/footer.php
@@ -29,31 +29,7 @@ $static_record = $model->fetchSelectedData('static_page_content', TABLE_STATIC_P
                                 </div>
                                 <div class="list-footer col-lg-8 col-md-8 col-sm-8 col-xs-12">
                                     <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 newslettter">    <div class="block block-subscribe">
-                                                <div class="block-title">
-                                                    <span>Newsletter Signup</span>
-                                                </div>
-                                                <form action="<?php echo base_url('index/saveNewsletter'); ?>" method="post" id="newsletter-validate-detail">
-                                                    <div class="block-content">
-                                                        <div class="form-subscribe-header">
-                                                            <div class="input-box">
-                                                                <input type="hidden" name="url" value="<?php echo current_url(); ?>"/>
-                                                                <input type="email" name="email" id="newsletter" title="Sign up for our newsletter" class="input-text required-entry validate-email"/>
-                                                            </div>
-                                                            <div class="actions">
-                                                                <button type="submit" title="Subscribe" class="button"><span><span>Subscribe</span></span></button>
-                                                            </div>
-                                                        </div>
-
-                                                    </div>
-                                                </form>
-                                                <script type="text/javascript">
-                                                    //<![CDATA[
-                                                    var newsletterSubscriberFormDetail = new VarienForm('newsletter-validate-detail');
-                                                    //]]>
-                                                </script>
-                                            </div>
-                                        </div>
+                                        <?php // echo $this->load->view('layout/front/footer-newsletter-subscribe'); ?>
                                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-links">
                                             <div class="footer-links">
                                                 <div class="row">
